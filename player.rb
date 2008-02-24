9d292c653e5d5e3a1bb2965f5af6fa884f53f0b0
Nick Brettell <nick@ordinateur.(none)>
2008-02-24 23:13:46 +1300

    Refactored and improved

--- player.rb
+++ player.rb
@@ -1,43 +1,34 @@
 class Player
   def initialize(game)
-    @state = 0
     @game = game
     @round = Round.new
   end
 
   def start_turn
-    if (@state == 0)
-      @game.roll
-      @state = @state + 1
-    end
+    @game.roll
   end
 
   def refine(reroll_dice)
-    if (@state > 0 && @state < 3)
-      @game.roll(reroll_dice)
-      @state = @state + 1
-    end
+    @game.roll(reroll_dice)
   end
 
   def score(type_string)
-    if (@state > 0)
-      @state = 0
-      case type_string
-      when "1" then @round.ones = @game.dice.map{|d| d.num}
-      when "2" then @round.twos = @game.dice.map{|d| d.num}
-      when "3" then @round.threes = @game.dice.map{|d| d.num}
-      when "4" then @round.fours = @game.dice.map{|d| d.num}
-      when "5" then @round.fives = @game.dice.map{|d| d.num}
-      when "6" then @round.sixes = @game.dice.map{|d| d.num}
-      when "s" then @round.small_straight = @game.dice.map{|d| d.num}
-      when "l" then @round.large_straight = @game.dice.map{|d| d.num}
-      when "h" then @round.full_house = @game.dice.map{|d| d.num}
-      when "t" then @round.three_of_a_kind = @game.dice.map{|d| d.num}
-      when "f" then @round.four_of_a_kind = @game.dice.map{|d| d.num}
-      when "y" then @round.yahtzee = @game.dice.map{|d| d.num}
-      when "?" then @round.chance = @game.dice.map{|d| d.num}
-      else -1
-      end
+    @game.end_turn
+    case type_string
+    when "1" then @round.ones = @game.dice
+    when "2" then @round.twos = @game.dice
+    when "3" then @round.threes = @game.dice
+    when "4" then @round.fours = @game.dice
+    when "5" then @round.fives = @game.dice
+    when "6" then @round.sixes = @game.dice
+    when "s" then @round.small_straight = @game.dice
+    when "l" then @round.large_straight = @game.dice
+    when "h" then @round.full_house = @game.dice
+    when "t" then @round.three_of_a_kind = @game.dice
+    when "f" then @round.four_of_a_kind = @game.dice
+    when "y" then @round.yahtzee = @game.dice
+    when "?" then @round.chance = @game.dice
+    else -1
     end
   end
 
--- yahtzee.rb
+++ yahtzee.rb
@@ -5,33 +5,53 @@ require 'player.rb'
 require 'round.rb'
 
 class YahtzeeGame
-  def initialize
+  def initialize(players = 1)
     @dice = [Die.new, Die.new, Die.new, Die.new, Die.new]
+    @no_of_players = players
+    @curr_player_no = 0
+    @players = Array.new(@no_of_players) { Player.new(self) }
+    @rolls = 0
   end
 
-  def roll(reroll_dice = @dice)
-    reroll_dice.each do | die | die.roll end
+  def roll(reroll_dice = [0,1,2,3,4])
+    if (@rolls < 3)
+      reroll_dice.map {|n| @dice[n] }.each do | die | die.roll end
+      @rolls = @rolls + 1
+    else
+      raise "You've had 3 rolls - choose a category to score this turn"
+    end
   end
 
-  attr_reader :dice
+  def end_turn
+    @rolls = 0
+    @curr_player_no = (@curr_player_no + 1) % @no_of_players
+  end
+
+  def curr_player
+    @players[@curr_player_no]
+  end
+
+  #attr_reader :dice
+  def dice
+    @dice.map {|d| d.num}
+  end
 end
 
-
 game = YahtzeeGame.new
-player = Player.new(game)
 while (true)
+  player = game.curr_player
   player.start_turn
-  puts "Your first roll is #{game.dice.map {|d| d.num}.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
+  puts "Your first roll is #{game.dice.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
   command = $stdin.gets
   reroll_dice = Array.new
-  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << game.dice[s-49] end }
+  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << s-49 end }
   player.refine(reroll_dice)
-  puts "Your second roll is #{game.dice.map {|d| d.num}.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
+  puts "Your second roll is #{game.dice.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
   command = $stdin.gets
   reroll_dice.clear
-  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << game.dice[s-49] end }
+  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << s-49 end }
   player.refine(reroll_dice)
-  puts "Your final roll is #{game.dice.map {|d| d.num}.join(', ')}.\nEnter your move - one of: 123456tfslh?y"
+  puts "Your final roll is #{game.dice.join(', ')}.\nEnter your move - one of: 123456tfslh?y"
   score = -1
   score = player.score($stdin.gets.strip) until (score != -1)
   player.print_board
