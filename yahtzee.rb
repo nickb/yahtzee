feee397768a9f603a9a30fc5aac1a3a06577b558
Nick Brettell <nbrettell@gmail.com>
2008-03-01 20:51:59 +1300

    Ability to set # of players as command line arg Fixed bug where game finished prematurely with multiple players Fixed bug when entering out of bounds dice to reroll

--- yahtzee.rb
+++ yahtzee.rb
@@ -40,7 +40,7 @@ class YahtzeeGame
   end
 
   def over?
-    @players.any? {|p| p.finished?}
+    !@players.any? {|p| !p.finished?}
   end
 end
 
@@ -50,7 +50,10 @@ key_move_mappings = {"1" => :ones, "2" => :twos, "3" => :threes,
     "t" => :three_of_a_kind, "f" => :four_of_a_kind,
     "h" => :full_house, "y" => :yahtzee, "?" => :chance};
 
-game = YahtzeeGame.new
+players = ARGV.first.to_i || 1
+players = 1 if (players <= 0)
+game = YahtzeeGame.new(players)
+puts "Beginning Yahtzee game with #{players} player#{players != 1 ? "s" : ""}"
 until (game.over?)
   puts "Player #{game.curr_player_no}'s turn"
   player = game.curr_player
@@ -58,12 +61,12 @@ until (game.over?)
   puts "Your first roll is #{game.dice.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
   command = $stdin.gets
   reroll_dice = Array.new
-  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << s-49 end }
+  command.each_byte {|s| if (s >= 49 && s < 54) then reroll_dice << s-49 end }
   player.refine(reroll_dice)
   puts "Your second roll is #{game.dice.join(', ')}.\nEnter the numbers of the dice you want to reroll (e.g. \"12345\" for all dice)"
   command = $stdin.gets
   reroll_dice.clear
-  command.each_byte {|s| if (s >= 49 && s <= 54) then reroll_dice << s-49 end }
+  command.each_byte {|s| if (s >= 49 && s < 54) then reroll_dice << s-49 end }
   player.refine(reroll_dice)
   puts "Your final roll is #{game.dice.join(', ')}.\nEnter your move - one of: 123456tfslh?y"
   score = -1
